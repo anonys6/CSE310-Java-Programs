b19700059b905f965a729985c5865d208f71038c
anonys6 <shakibalam998@gmail.com>
2024-01-25 12:13:53 +0530

    adding lessons of operator

--- 2_operator/operator.java
+++ 2_operator/operator.java
@@ -2,15 +2,23 @@ import java.util.Scanner;
 
 public class operator {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        // Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Enter three numbers:");
-        int x = scanner.nextInt();
-        int y = scanner.nextInt();
-        int z = scanner.nextInt();
+        // System.out.println("Enter three numbers:");
+        // int x = scanner.nextInt();
+        // int y = scanner.nextInt();
+        // int z = scanner.nextInt();
 
-        int max = x > y ? (x > z ? x : z) : (y > z ? y : z);
+        // int max = x > y ? (x > z ? x : z) : (y > z ? y : z);
 
-        System.out.println("The maximum number is " + max);
+        // System.out.println("The maximum number is " + max);
+
+        // scanner.close();
+        
+
+        // int num = -8;
+        int res = -8 >>> 2;
+
+        System.out.println(res);
     }
 }
