da8b58aeffe02b4c96eb9d7eccbf995d5d49dc4b
anonys6 <shakibalam998@gmail.com>
2024-03-29 14:00:52 +0530

    adding all the java files

---
+++ 4_java_date_time/LocalDatePkg.java
@@ -0,0 +1,11 @@
+// package 4_java_date_time;
+
+import java.time.LocalDate;
+
+public class LocalDatePkg {
+    public static void main(String[] args) {
+        LocalDate myDate = LocalDate.now();
+
+        System.out.println(myDate);
+    }
+}
