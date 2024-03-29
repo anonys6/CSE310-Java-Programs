86d04536e3d2c19c4c44b2adc07bccd25a9a1c8f
anonys6 <shakibalam998@gmail.com>
2024-03-29 14:01:57 +0530

    fixing all the warnings

--- 1_hello/hello.java
+++ 1_hello/hello.java
@@ -1,8 +1,5 @@
 public class hello {
     public static void main(String[] args) {
         System.out.println("Hello World!");
-
-        int x = 5;
-        
     }
 }
--- 3_sorting/Fre.java
+++ 3_sorting/Fre.java
@@ -1,7 +1,7 @@
 import java.util.Map;
 import java.util.HashMap;
-import java.util.Collections;
-import java.util.Arrays;
+// import java.util.Collections;
+// import java.util.Arrays;
 
 public class Fre {
     public static void main(String[] args) {
